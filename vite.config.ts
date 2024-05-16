d12898324c0a7d15e938a84bd69fa9e783704fcf
motoshira <51407064+motoshira@users.noreply.github.com>
2024-05-16 22:24:55 +0900

    Configure build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import { resolve } from "path";
+
+const root = resolve(__dirname, ".");
+const outDir = resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-})
+  root,
+  build: {
+    outDir,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        "001": resolve(root, "001", "index.html"),
+      },
+    },
+  },
+});
